b9368a79e850682be7a1cf0e2dbe2da4d0d98916
Asatryan Georgy <ago-2004@mail.ru>
2024-12-12 03:25:23 +0300

    WIP

--- hw2/raft.cpp
+++ hw2/raft.cpp
@@ -3,6 +3,9 @@
 
 #include "raft.pb.h"
 
+#include <cassert>
+#include <chrono>
+#include <cstddef>
 #include <grpcpp/support/status.h>
 
 #include <atomic>
@@ -10,15 +13,20 @@
 #include <map>
 #include <memory>
 #include <mutex>
+#include <optional>
+#include <queue>
 #include <stdexcept>
 #include <thread>
+#include <tuple>
 
 namespace hw2 {
 
 namespace raft {
 
 class NodeImpl {
-    static const Node::Id totalIds = 3;
+    static constexpr auto ELECTION_TIMEOUT = std::chrono::seconds(5);
+    static constexpr auto HEARTBEAT_INTERVAL = std::chrono::seconds(1);
+    static constexpr Node::Id TOTAL_IDS = 3;
 
   public:
     NodeImpl(Node::Id id) : networkNode_(*this, id), id_(id) {
@@ -27,7 +35,16 @@ class NodeImpl {
     };
 
     Node::Id ThisId() { return id_; }
-    std::optional<Node::Id> LeaderId() { return leaderId_; }
+
+    std::optional<Node::Id> LeaderId() {
+        switch (status_) {
+        case Status::LEADER:
+        case Status::FOLLOWER:
+            return votedFor;
+        default:
+            return std::nullopt;
+        }
+    }
 
     void Get(KeyT key, Node::CallbackGetT callback) {
         throw std::runtime_error("Not implemented NodeImpl::Get");
@@ -48,6 +65,16 @@ class NodeImpl {
     }
 
   private:
+    void Apply(::raft::proto::WALEntry entry) {
+        throw std::runtime_error("Not implemented");
+        //
+    }
+
+    void Heartbeat() {
+        throw std::runtime_error("Not implemented");
+        //
+    }
+
     void Initialize() {
         workThread_ = std::thread{[this]() {
             while (doWork_) {
@@ -58,38 +85,30 @@ class NodeImpl {
     }
 
     void Work() {
-        while (true) {
-            std::cout << "Sending heartbeats!\n";
-            for (Node::Id id = 1; id <= totalIds; ++id) {
-                if (id != id_) {
-                    std::mutex mu;
-                    std::condition_variable cv;
-                    bool done = false;
-                    ::raft::proto::RequestVoteRequest request;
-                    request.set_term(id_ * 10 + id);
-                    networkNode_.SendRequestVote(
-                        id, request,
-                        [&](grpc::Status status, ::raft::proto::RequestVoteResponse response) {
-                            if (status.ok()) {
-                                std::cout << "Succesfully performed RequestVote rpc on node " << id
-                                          << '\n';
-                                std::cout << "Response" << response.term() << " from node " << id
-                                          << '\n';
-                            } else {
-                                std::cout << "Couldn't perform RequestVote rpc on node " << id
-                                          << '\n';
-                            }
-                            std::lock_guard lock(mu);
-                            done = true;
-                            cv.notify_one();
-                        });
-                    std::unique_lock lock(mu);
-                    cv.wait(lock, [&done] { return done; });
-                }
-            }
-            sleep(5);
+        if (status_ != Status::CANDIDATE &&
+            std::chrono::system_clock::now() - lastLeaderTime > ELECTION_TIMEOUT) //
+        {
+            ConvertToCandidate();
         }
-        throw std::runtime_error("Not implemented NodeImpl::Work");
+        while (commitIndex > lastAppliedIndex) {
+            Apply(log[lastAppliedIndex++]);
+        }
+        if (status_ == Status::LEADER &&
+            std::chrono::system_clock::now() - lastHeartbeatTime > HEARTBEAT_INTERVAL) {
+            Heartbeat();
+        }
+    }
+
+    void ConvertToFollower() {
+        status_ = Status::FOLLOWER;
+        votedFor = std::nullopt;
+    }
+
+    void ConvertToCandidate() {
+        status_ = Status::CANDIDATE;
+        votedFor = ThisId();
+        votesReceived = 1;
+        throw std::runtime_error("Not implemented");
     }
 
     class NetworkNodeImpl : public network::Node {
@@ -101,24 +120,104 @@ class NodeImpl {
 
         void HandleRequestVote(Id from, const ::raft::proto::RequestVoteRequest *request,
                                ::raft::proto::RequestVoteResponse *response) override {
-            std::cout << "Received heartbeat from " << from << "!\n";
-            response->set_term(request->term() * 100 + 10 * id_ + from);
-            // throw std::runtime_error("Not implemented NetworkNodeImpl::HandleRequestVote");
+            if (request->term() < raftNode_.currentTerm) {
+                response->set_votegranted(false);
+                response->set_term(raftNode_.currentTerm);
+                return;
+            }
+            if (request->term() > raftNode_.currentTerm) {
+                raftNode_.currentTerm = request->term();
+                raftNode_.ConvertToFollower();
+            }
+            if (raftNode_.votedFor.value_or(from) == from) {
+                if (raftNode_.log.empty() ||
+                    std::make_pair(raftNode_.log.back().term(), raftNode_.log.size()) <=
+                        std::make_pair(request->lastlogterm(), request->lastlogindex())) //
+                {
+                    raftNode_.votedFor = from;
+                    response->set_votegranted(true);
+                    response->set_term(raftNode_.currentTerm);
+                    raftNode_.lastLeaderTime = std::chrono::system_clock::now();
+                    return;
+                }
+            }
+            response->set_votegranted(false);
+            response->set_term(raftNode_.currentTerm);
+            return;
         };
 
         void HandleAppendEntries(Id from, const ::raft::proto::AppendEntriesRequest *request,
                                  ::raft::proto::AppendEntriesResponse *response) override {
-            throw std::runtime_error("Not implemented NetworkNodeImpl::HandleAppendEntries");
+            if (request->term() < raftNode_.currentTerm) {
+                response->set_success(false);
+                response->set_term(raftNode_.currentTerm);
+                return;
+            }
+            if (request->term() > raftNode_.currentTerm) {
+                raftNode_.currentTerm = request->term();
+                raftNode_.ConvertToFollower();
+                raftNode_.votedFor = from;
+                raftNode_.lastLeaderTime = std::chrono::system_clock::now();
+            }
+            if (raftNode_.log.size() < request->prevlogindex() ||
+                (request->prevlogindex() != 0 &&
+                 raftNode_.log.at(request->prevlogindex() - 1).term())) //
+            {
+                response->set_success(false);
+                response->set_term(raftNode_.currentTerm);
+                response->set_lastreplicatedindex(raftNode_.commitIndex);
+                return;
+            }
+            size_t curIndex = request->prevlogindex();
+            for (const auto &entry : request->entries()) {
+                if (raftNode_.log.size() == curIndex) {
+                    raftNode_.log.push_back(entry);
+                } else {
+                    assert(raftNode_.log.size() > curIndex);
+                    if (raftNode_.log.at(curIndex).term() != entry.term()) {
+                        raftNode_.log.at(curIndex) = entry;
+                    }
+                }
+                curIndex++;
+            }
+            if (request->leadercommitindex() > raftNode_.commitIndex) {
+                raftNode_.commitIndex = std::min(request->leadercommitindex(), curIndex);
+            }
+            raftNode_.lastLeaderTime = std::chrono::system_clock::now();
         }
 
       private:
         NodeImpl &raftNode_;
     } networkNode_;
+    // friend class NetworkNodeImpl;
 
     Node::Id id_;
-    std::optional<Node::Id> leaderId_;
 
     std::map<KeyT, ValT> state_;
+
+    enum class Status {
+        FOLLOWER,
+        CANDIDATE,
+        LEADER,
+    } status_ = Status::FOLLOWER;
+
+    size_t currentTerm = 0;
+    std::optional<Node::Id> votedFor;
+    std::vector<::raft::proto::WALEntry> log;
+    size_t commitIndex = 0;
+    size_t lastAppliedIndex = 0;
+
+    decltype(std::chrono::system_clock::now()) lastLeaderTime = std::chrono::system_clock::now();
+
+    // leaderOnly---
+    std::map<Node::Id, size_t> nextIndex;
+    std::map<Node::Id, size_t> matchIndex;
+    decltype(std::chrono::system_clock::now()) lastHeartbeatTime = std::chrono::system_clock::now();
+    // -------------
+    // candidateOnly
+    std::size_t votesReceived = 0;
+    // -------------
+
     std::atomic_bool doWork_ = true;
     std::thread workThread_;
 };
